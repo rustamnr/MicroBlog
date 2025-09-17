00c74bccc27b8785101cb8d97b244080c7aa816c
lsmltesting <777igorryan777@gmail.com>
2025-09-17 17:48:31 +0300

    changing service files

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	microblog "github.com/lsmltesting/MicroBlog"
+)
+
+func main() {
+	server := new(microblog.Server)
+	if err := server.Run("8000"); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
 }
---
+++ internal/service/user_repo.go
@@ -0,0 +1,48 @@
+package service
+
+import (
+	"sync"
+
+	customErrors "github.com/lsmltesting/MicroBlog/internal/errors"
+	"github.com/lsmltesting/MicroBlog/internal/models"
+)
+
+type UserRepository interface {
+	Save(user *models.User) (int, error)
+	FindUserById(id int) (*models.User, error)
+}
+
+type inMemoryUserRepo struct {
+	mtx    sync.RWMutex
+	data   map[int]*models.User
+	lastID int
+}
+
+func NewInMemoryUserRepo() UserRepository {
+	return &inMemoryUserRepo{
+		data: make(map[int]*models.User),
+	}
+}
+
+func (r *inMemoryUserRepo) Save(user *models.User) (int, error) {
+	r.mtx.Lock()
+	defer r.mtx.Unlock()
+
+	r.lastID++
+	user.ID = r.lastID
+	r.data[r.lastID] = user
+
+	return r.lastID, nil
+}
+
+func (r *inMemoryUserRepo) FindUserById(id int) (*models.User, error) {
+	r.mtx.RLock()
+	defer r.mtx.Unlock()
+
+	user, ok := r.data[id]
+	if !ok {
+		return nil, customErrors.ErrNotFindUser
+	}
+
+	return user, nil
+}
